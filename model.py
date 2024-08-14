b672fd7bfd652322be426de501197a939d98279a
Cristian Guarnizo-Lemus <cdguarnizo@gmail.com>
2024-08-13 22:21:14 -0500

    Update model.py

--- model.py
+++ model.py
@@ -12,7 +12,46 @@ class LeakyReLU(layers.Layer):
     def call(self, X):
         return tf.maximum(X, self.leak * X)
 
-class GAN(Model):
+def batchnormalize(X, eps=1e-8, g=None, b=None):
+    if X.get_shape().ndims == 4:
+        mean = tf.reduce_mean(X, [0,1,2])
+        std = tf.reduce_mean( tf.square(X-mean), [0,1,2] )
+        X = (X-mean) / tf.sqrt(std+eps)
+
+        if g is not None and b is not None:
+            g = tf.reshape(g, [1,1,1,-1])
+            b = tf.reshape(b, [1,1,1,-1])
+            X = X*g + b
+
+    elif X.get_shape().ndims == 2:
+        mean = tf.reduce_mean(X, 0)
+        std = tf.reduce_mean(tf.square(X-mean), 0)
+        X = (X-mean) / tf.sqrt(std+eps)
+
+        if g is not None and b is not None:
+            g = tf.reshape(g, [1,-1])
+            b = tf.reshape(b, [1,-1])
+            X = X*g + b
+
+    else:
+        raise NotImplementedError
+
+    return X
+
+def lrelu(X, leak=0.2):
+    f1 = 0.5 * (1 + leak)
+    f2 = 0.5 * (1 - leak)
+    return f1 * X + f2 * tf.abs(X)
+
+def bce(o, t):
+    o = tf.clip_by_value(o, 1e-7, 1. - 1e-7)
+    return tf.math.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(o, t))
+
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+class GAN():
     def __init__(
             self,
             batch_size=32,
@@ -25,7 +64,6 @@ class GAN(Model):
             dim_channel=1,
             ):
 
-        super(GAN, self).__init__()
         self.batch_size = batch_size
         self.image_shape = image_shape
         self.dim_z = dim_z
@@ -35,59 +73,61 @@ class GAN(Model):
         self.dim_W2 = dim_W2
         self.dim_W3 = dim_W3
         self.dim_channel = dim_channel
+        self.gen_W1 = tf.Variable(tf.random.normal([dim_z+dim_y, dim_W1], stddev=0.02), name='gen_W1')
+        self.gen_W2 = tf.Variable(tf.random.normal([dim_W1+dim_y, dim_W2*6*6], stddev=0.02), name='gen_W2')
+        self.gen_W3 = tf.Variable(tf.random.normal([5,5,dim_W3,dim_W2+dim_y], stddev=0.02), name='gen_W3')
+        self.gen_W4 = tf.Variable(tf.random.normal([5,5,dim_channel,dim_W3+dim_y], stddev=0.02), name='gen_W4')
 
-        self.gen_W1 = layers.Dense(dim_W1, use_bias=False, name='gen_W1')
-        self.gen_W2 = layers.Dense(dim_W2 * 6 * 6, use_bias=False, name='gen_W2')
-        self.gen_conv2d_transpose1 = layers.Conv2DTranspose(filters=dim_W3, kernel_size=5, strides=2, padding='same', use_bias=False, name='gen_conv2d_transpose1')
-        self.gen_conv2d_transpose2 = layers.Conv2DTranspose(filters=dim_channel, kernel_size=5, strides=2, padding='same', use_bias=False, name='gen_conv2d_transpose2')
+        self.discrim_W1 = tf.Variable(tf.random.normal([5,5,dim_channel+dim_y,dim_W3], stddev=0.02), name='discrim_W1')
+        self.discrim_W2 = tf.Variable(tf.random.normal([5,5,dim_W3+dim_y,dim_W2], stddev=0.02), name='discrim_W2')
+        self.discrim_W3 = tf.Variable(tf.random.normal([dim_W2*6*6+dim_y,dim_W1], stddev=0.02), name='discrim_W3')
+        self.discrim_W4 = tf.Variable(tf.random.normal([dim_W1+dim_y,1], stddev=0.02), name='discrim_W4')
 
-        self.discrim_conv2d_1 = layers.Conv2D(filters=dim_W3, kernel_size=5, strides=2, padding='same', use_bias=False, name='discrim_conv2d_1')
-        self.discrim_conv2d_2 = layers.Conv2D(filters=dim_W2, kernel_size=5, strides=2, padding='same', use_bias=False, name='discrim_conv2d_2')
-        self.discrim_W3 = layers.Dense(dim_W1, use_bias=False, name='discrim_W3')
-        self.discrim_W4 = layers.Dense(1, use_bias=False, name='discrim_W4')
-        self.leaky_relu = LeakyReLU()
+    def build_model(self):
+        Z = tf.keras.Input(name='Z', shape=(self.dim_z,), dtype=tf.dtypes.float32)
+        Y = tf.keras.Input(name='Y', shape=(self.dim_y,), dtype=tf.dtypes.float32)
+        image_real = tf.keras.Input(name='image_real', shape=self.image_shape, dtype=tf.dtypes.float32)
 
-    def batchnormalize(self, X, epsilon=1e-8):
-        return layers.BatchNormalization()(X)
-
-    def call(self, inputs):
-        Z, Y, image_real = inputs
         h4 = self.generate(Z, Y)
-        image_gen = layers.Activation('sigmoid')(h4)
+        image_gen = tf.nn.sigmoid(h4)
 
         raw_real2 = self.discriminate(image_real, Y)
+        p_real = tf.reduce_mean(raw_real2)
+
         raw_gen2 = self.discriminate(image_gen, Y)
+        p_gen = tf.reduce_mean(raw_gen2)
 
-        p_real = layers.Lambda(lambda x: tf.reduce_mean(x))(raw_real2)
-        p_gen = layers.Lambda(lambda x: tf.reduce_mean(x))(raw_gen2)
+        discrim_cost = tf.reduce_sum(raw_real2) - tf.reduce_sum(raw_gen2)
+        gen_cost = -tf.reduce_mean(raw_gen2)
 
-        discrim_cost = layers.Lambda(lambda x: tf.reduce_sum(x[0]) - tf.reduce_sum(x[1]))([raw_real2, raw_gen2])
-        gen_cost = layers.Lambda(lambda x: -tf.reduce_mean(x))(raw_gen2)
-
-        return discrim_cost, gen_cost, p_real, p_gen
+        return Z, Y, image_real, discrim_cost, gen_cost, p_real, p_gen
 
     def discriminate(self, image, Y):
         yb = layers.Reshape([1, 1, self.dim_y])(Y)
         X = layers.Concatenate(axis=3)([image, yb * tf.ones([self.batch_size, 24, 24, self.dim_y])])
-        h1 = self.leaky_relu(self.discrim_conv2d_1(X))
+        h1 = lrelu(tf.nn.conv2d(X, self.discrim_W1, strides=[1,2,2,1], padding='SAME'))
         h1 = layers.Concatenate(axis=3)([h1, yb * tf.ones([self.batch_size, 12, 12, self.dim_y])])
-        h2 = self.leaky_relu(self.batchnormalize(self.discrim_conv2d_2(h1)))
-        h2 = layers.Flatten()(h2)
+        h2 = lrelu(batchnormalize(tf.nn.conv2d(h1, self.discrim_W2, strides=[1,2,2,1], padding='SAME')))
+        h2 = tf.reshape(h2, [self.batch_size, -1])
         h2 = layers.Concatenate(axis=1)([h2, Y])
-        h3 = self.leaky_relu(self.batchnormalize(self.discrim_W3(h2)))
+        h3 = lrelu(batchnormalize(tf.matmul(h2, self.discrim_W3)))
         return h3
 
     def generate(self, Z, Y):
-        Z = layers.Concatenate(axis=1)([Z, Y])
-        h1 = layers.ReLU()(self.batchnormalize(self.gen_W1(Z)))
-        h1 = layers.Concatenate(axis=1)([h1, Y])
-        h2 = layers.ReLU()(self.batchnormalize(self.gen_W2(h1)))
-        h2 = layers.Reshape([6, 6, self.dim_W2])(h2)
         yb = layers.Reshape([1, 1, self.dim_y])(Y)
+        Z = layers.Concatenate(axis=1)([Z, Y])
+        h1 = tf.nn.relu(batchnormalize(tf.matmul(Z, self.gen_W1)))
+        h1 = layers.Concatenate(axis=1)([h1, Y])
+        h2 = tf.nn.relu(batchnormalize(tf.matmul(h1, self.gen_W2)))
+        h2 = tf.reshape(h2, [self.batch_size, 6, 6, self.dim_W2])
         h2 = layers.Concatenate(axis=3)([h2, yb * tf.ones([self.batch_size, 6, 6, self.dim_y])])
 
-        h3 = layers.ReLU()(self.batchnormalize(self.gen_conv2d_transpose1(h2)))
+        output_shape_l3 = [self.batch_size, 12, 12, self.dim_W3]
+        h3 = tf.nn.conv2d_transpose(h2, self.gen_W3, output_shape=output_shape_l3, strides=[1, 2, 2, 1])
+        h3 = tf.nn.relu(batchnormalize(h3))
         h3 = layers.Concatenate(axis=3)([h3, yb * tf.ones([self.batch_size, 12, 12, self.dim_y])])
 
-        h4 = self.gen_conv2d_transpose2(h3)
+        output_shape_l4 = [self.batch_size, 24, 24, self.dim_channel]
+        h4 = tf.nn.conv2d_transpose(h3, self.gen_W4, output_shape=output_shape_l4, strides=[1, 2, 2, 1])
         return h4
+
